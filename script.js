351642ce5da88a7b0fad08b8189c79b13addd10a
kushalkumar1362 <kushalkumar1362@gmail.com>
2024-02-10 14:31:08 +0530

    In main Section add SearchBar

--- script.js
+++ script.js
@@ -3,33 +3,58 @@ const get = (param) => document.getElementById(`${param}`);
 const btnmode = get("btn-mode");
 const modetext = get("mode-text");
 const modeicon = get("mode-icon");
+const searchbar = document.querySelector(".searchbar-container");
+const btnsubmit = get("submit");
+const input = get("input");
 
 // Intially
 let darkMode = false;
 
+btnsubmit.addEventListener("click", function () {
+    if (input.value.trim() !== "") {
+        searchbar.style.border = "1px solid green";
+        name_error.style.display = "none";
+    } else {
+        searchbar.style.border = "1px solid red";
+        name_error.style.display = "block";
+        input.focus();
+    }
+});
+
+input.addEventListener("input", () => {
+    if (input.value.trim() !== "") {
+        searchbar.style.border = "1px solid green";
+        name_error.style.display = "none";
+    }
+});
+
 // Listner for mode button
 btnmode.addEventListener("click", function () {
-  if (darkMode == false) {
-    darkModeProperties();
-  } else {
-    lightModeProperties();
-  }
+    if (darkMode == false) {
+        darkModeProperties();
+    } else {
+        lightModeProperties();
+    }
 });
 
 function darkModeProperties() {
-  root.setProperty("--lm-bg", "#141D2F");
-  root.setProperty("--lm-text", "white");
-  modetext.innerText = "LIGHT";
-  modeicon.src = "./Assets/sun-icon.svg";
-  darkMode = true;
-  localStorage.setItem("dark-mode", true);
+    root.setProperty("--lm-bg", "#141D2F");
+    root.setProperty("--lm-text", "white");
+    root.setProperty("--lm-bg-content", "#1E2A47");
+    root.setProperty("--lm-shadow-xl", "rgba(70,88,109,0.15)");
+    modetext.innerText = "LIGHT";
+    modeicon.src = "./Assets/sun-icon.svg";
+    darkMode = true;
+    localStorage.setItem("dark-mode", true);
 }
 
 function lightModeProperties() {
-  root.setProperty("--lm-bg", "#F6F8FF");
-  root.setProperty("--lm-text", "#4B6A9B");
-  modetext.innerText = "DARK";
-  modeicon.src = "./Assets/moon-icon.svg";
-  darkMode = false;
-  localStorage.setItem("dark-mode", false);
+    root.setProperty("--lm-bg", "#F6F8FF");
+    root.setProperty("--lm-text", "#4B6A9B");
+    root.setProperty("--lm-bg-content", "#FEFEFE");
+    root.setProperty("--lm-shadow-xl", "rgba(70, 88, 109, 0.25)");
+    modetext.innerText = "DARK";
+    modeicon.src = "./Assets/moon-icon.svg";
+    darkMode = false;
+    localStorage.setItem("dark-mode", false);
 }
